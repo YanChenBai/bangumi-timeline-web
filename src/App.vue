6428a3eef85206ca7ab850814241928bbb603f31
BaiYanchen <2786660867@qq.com>
2024-06-20 15:47:15 +0800

    feat: ✨ 添加每日选项卡标题的点击滚动

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-  <n-config-provider
-    abstract
-    :theme-overrides="{
-      Popover: {
-        borderRadius: '1rem',
-        padding: '10px'
-      }
-    }"
-  >
+  <n-config-provider abstract :theme-overrides="{
+    Popover: {
+      borderRadius: '1rem',
+      padding: '10px'
+    }
+  }">
     <div class="min-h-screen w-full bg-[#EDF1F2] relative">
       <div class="container m-auto px-[20px] box-border">
         <RouterView />
--- src/components/DayCard.vue
+++ src/components/DayCard.vue
@@ -3,7 +3,7 @@
     <div class="mb-[10px]" :id="`week-${day}`">
       <div class="w-full rounded-full flex justify-center p1.5 box-border relative">
         <TimelineItem class="relative z-2" :activate="true" :prefix="Prefixs[day]" :color="DayColors[day]"
-          :content="Days[day]"></TimelineItem>
+          :content="Days[day]" @click="onClick"></TimelineItem>
         <div class="absolute w-full h2px top-[calc(50%-1px)]" :style="{ background: DayColors[day] }"></div>
       </div>
     </div>
@@ -19,12 +19,17 @@ import type { Bangumi } from '@/types'
 import BangumiCard from './BangumiCard.vue'
 import TimelineItem from './TimelineItem.vue'
 import { DayColors, Days, Prefixs } from '@/shareds/configs'
+import { scrollToElement } from '@/shareds/scrollToElement';
 
 defineOptions({ name: 'DayCard' })
-defineProps<{
+const props = defineProps<{
   data: Bangumi[]
   day: number
 }>()
+
+function onClick() {
+  scrollToElement(`week-${props.day}`, -60)
+}
 </script>
 
 <style scoped>
