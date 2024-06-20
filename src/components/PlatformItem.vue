7fb7e22f2cde963eaa90dcbc031334b281ed4c0c
byc <2786660867@qq.com>
2024-06-20 20:15:42 +0800

    fix: 🐛 修复白色主题下文字颜色异常

--- src/components/PlatformItem.vue
+++ src/components/PlatformItem.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="platform-menu-item"
-    :style="{ color: activate ? '#fff' : color, background: activate ? color : 'transparent' }">
+  <div
+    class="platform-menu-item"
+    :style="{ color: activate ? '#fff' : color, background: activate ? color : 'transparent' }"
+  >
     <div class="h-4 flex p1 rounded-full" :class="[themeClass('bg-white', 'bg-#212126')]">
       <img :src="logo" alt="logo" class="h-full" />
     </div>
-    <div class="flex font-800 h-5 leading-5"
-      :class="[themeClass('text-#fff', activate ? 'text-#212126' : 'text-white/80')]">{{ name
-      }}
+    <div
+      class="flex font-800 h-5 leading-5"
+      :class="[
+        themeClass(
+          activate ? 'text-#fff' : 'text-black',
+          activate ? 'text-#212126' : 'text-white/80'
+        )
+      ]"
+    >
+      {{ name }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useTheme } from '@/hooks/useTheme';
+import { useTheme } from '@/hooks/useTheme'
 
 defineOptions({ name: 'PlatformItem' })
 defineProps<{
