b0710adf4afd536c7b4f7e14239b1a2c107dd57c
byc <2786660867@qq.com>
2024-06-24 22:41:41 +0800

    feat: ✨ 添加配置和logo

--- src/shareds/configs.ts
+++ src/shareds/configs.ts
@@ -5,11 +5,12 @@ export const DayColors = [
   '#32B16C',
   '#00B8EE',
   '#546FB4',
-  '#8956A1'
+  '#8956A1',
+  '#EF4D4D'
 ]
 
-export const Days = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
-export const Prefixs = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+export const Days = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日', '其他']
+export const Prefixs = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Oth']
 
 export const PlatformColors: Record<string, string> = {
   tencent: '#0098FF',
@@ -18,5 +19,6 @@ export const PlatformColors: Record<string, string> = {
   tl5dm: '#FF5050',
   bangumitv: '#F29BA3',
   yucwiki: '#CCA4A2',
-  acgsecrets: '#F8C3CD'
+  acgsecrets: '#F8C3CD',
+  anilist: '#3577FF'
 }
