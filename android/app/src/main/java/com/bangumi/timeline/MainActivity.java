97af1074ab6d8874fc46232ea04d279b5dd8153f
BaiYanchen <2786660867@qq.com>
2024-07-03 17:30:29 +0800

    feat: ✨ 添加capacitorjs打包成apk

---
+++ android/app/src/main/java/com/bangumi/timeline/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bangumi.timeline;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bangumi.timeline',
+  appName: 'bangumi-timeline-web',
+  webDir: 'dist'
+};
+
+export default config;
